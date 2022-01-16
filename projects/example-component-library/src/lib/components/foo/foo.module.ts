634ffab7d931acc5264645f3d8d0b7f6f6cc23ad
Mutasem Ahmad <Mutasem.Ahmad@b12app.com>
2022-01-17 01:11:21 +0200

    initial commit

--- projects/example-component-library/src/lib/components/foo/foo.module.ts
+++ projects/example-component-library/src/lib/components/foo/foo.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooComponent } from './foo.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 @NgModule({
   declarations: [FooComponent],
-  imports: [CommonModule, NzButtonModule],
+  imports: [CommonModule, NzButtonModule, NzTableModule, NzDividerModule],
   exports: [FooComponent],
 })
 export class FooModule {}
