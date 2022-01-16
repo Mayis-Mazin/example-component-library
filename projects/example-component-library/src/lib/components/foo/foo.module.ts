f15f590c2276e05d390b0750995a28e61ffb0e9f
Mutasem Ahmad <Mutasem.Ahmad@b12app.com>
2022-01-17 00:58:32 +0200

    add ng zorro without understand anything

--- projects/example-component-library/src/lib/components/foo/foo.module.ts
+++ projects/example-component-library/src/lib/components/foo/foo.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooComponent } from './foo.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
   declarations: [FooComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NzButtonModule],
   exports: [FooComponent],
 })
 export class FooModule {}
